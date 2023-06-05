c5a4dbf5f300849853646107e0b22ac6446e1424
simone dainese <s.daineselibero@gmail.com>
2023-06-05 14:53:43 +0200

    a

--- src/components/Products/ProductSidebar.jsx
+++ src/components/Products/ProductSidebar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "../../pages/Products/products.css";
+import {IoIosArrowDown} from "react-icons/io"
 
 const ProductSidebar = () => {
     return (
         <section className="productSidebar">
-            
+                <div className="containerCardProductMenu">
+                    <p>Gender</p>
+                    <IoIosArrowDown/>
+                </div>
+                <div>
+                    
+                </div>
+                <p>Man</p>
+                <p>Woman</p>
+                <p>Kids</p>
+                <div className="divisiorio"></div>
+                <p>T-shirt</p>
+                <p>Polo</p>
+                <p>Shirt</p>
+                <p>Hoodies</p>
+                <p>Tank-top</p>
+                <div className="divisiorio"></div>
         </section>
     )
 }
