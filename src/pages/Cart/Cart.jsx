2eda6ddde506ecc38c1ed129b3a01cd1ab835e34
MrRobot996 <guiseddu@gmail.com>
2023-06-05 15:19:07 +0200

    0

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -8,6 +8,10 @@ const Cart = () => {
   return (
     <>
       <Navbar />
+      <div className="cartContainer">
+        <div className="leftPartCart"></div>
+        <div className="rightPartCart"></div>
+      </div>
       <TopReminder />
       <Footer />
     </>
