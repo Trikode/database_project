21c3a2e7ff930fa50c8c4bfbafcf2b055f736a9b
AresBoscaro <124804516+AresBoscaro@users.noreply.github.com>
2023-06-19 14:13:52 +0200

    a

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,48 +1,54 @@
 import React from "react";
-import { useState} from "react";
-import "./account.css"
+import { useState } from "react";
+import "./account.css";
 import LoginForm from "../../components/Account/LoginForm";
 import RegistrationForm from "../../components/Account/RegistrationForm";
 // import UserList from "../../components/Account/Testaccount";
 
-const Account = ()=>{
-    const [isActive, setIsActive] = useState(true);
-    // const [users, setUsers] = useState([]);
-    
+const Account = () => {
+  const [isActive, setIsActive] = useState(true);
+  // const [users, setUsers] = useState([]);
 
-    // useEffect(() => {
-    //   fetch('http://localhost:3308/api/users')
-    //     .then((response) =>response.json())
-    //     .then((data) => setUsers(data))
-    //     .catch((error) => console.error('Error:', error));
-    // }, []);  
-    // console.log(users)
+  // useEffect(() => {
+  //   fetch('http://localhost:3308/api/users')
+  //     .then((response) =>response.json())
+  //     .then((data) => setUsers(data))
+  //     .catch((error) => console.error('Error:', error));
+  // }, []);
+  // console.log(users)
 
-    return(
+  return (
     <div className="AccountContainer">
-        <div className="Acontainer"> 
-            <div className="Alabels">
-                <button onClick={()=>setIsActive(true)}className={ `loginL ${isActive ? "active" : ""}`}><h2 >Login</h2></button>
-                <button onClick={()=>setIsActive(false)}className={ `regL ${isActive ? "" : "active"}`}><h2 >Register</h2></button>
-            </div>
-            
-            <div className="AccountForms">
-                {isActive?<LoginForm/>:<RegistrationForm/>}
-            </div>
-            <div>
-      {/* <h1>Users</h1>
+      <div className="Acontainer">
+        <div className="Alabels">
+          <button
+            onClick={() => setIsActive(true)}
+            className={`loginL ${isActive ? "active" : ""}`}
+          >
+            <h2>Login</h2>
+          </button>
+          <button
+            onClick={() => setIsActive(false)}
+            className={`regL ${isActive ? "" : "active"}`}
+          >
+            <h2>Register</h2>
+          </button>
+        </div>
+
+        <div className="AccountForms">
+          {isActive ? <LoginForm /> : <RegistrationForm />}
+        </div>
+        <div>
+          {/* <h1>Users</h1>
       <ul>
         {users.map((user,Uidx) =>{ 
             return <UserList key={Uidx} email={user.email} psw={user.password}/>
         })}
       </ul> */}
-    </div>
         </div>
-       
+      </div>
     </div>
-       
-   
-    );
+  );
 };
 
-export default Account
+export default Account;
