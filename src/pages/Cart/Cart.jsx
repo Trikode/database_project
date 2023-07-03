1b75290ab8229bc124ed4bfae2dc868d916c3c47
MrRobot996 <guiseddu@gmail.com>
2023-07-03 16:05:47 +0200

    .

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -15,81 +15,141 @@ const Cart = () => {
   const { currentUser } = useLiveItems();
   const [deliveries, setDeliveries] = useState([]);
   const [carts, setCarts] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [cartOverlay, setCartOverlay] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:3308/api/carts?userId=${currentUser.id_user}`)
       .then((response) => response.json())
-      .then((data) => setCarts(data))
+      .then((data) => {
+        setCarts(data);
+        // Calculate the total price
+        const totalPrice = data.reduce((acc, item) => acc + item.price, 0);
+        setTotalPrice(totalPrice);
+      })
       .catch((error) => console.error("Error:", error));
   }, [currentUser.id_user]);
+
   useEffect(() => {
     fetch("http://localhost:3308/api/deliveries")
       .then((response) => response.json())
       .then((data) => setDeliveries(data))
       .catch((error) => console.error("Error:", error));
   }, []);
+
   // const user = deliveries.find((user) => user.id_user === currentUser.id_user);
   return (
     <>
       <TopReminder />
       <Navbar />
-      <div class="CartContainer">
-        <div class="CartWindow">
-          <div class="CartOrder-info">
-            <div class="CartOrder-info-content">
-              <h2>Riepilogo Ordine</h2>
-              <div className="CartItemsContainer">
-                {carts.map((item) => {
-                  return (
-                    <CartItem
-                      img={item.image_url}
-                      name={item.name}
-                      size={item.size}
-                      color={item.color}
-                      price={item.price}
-                    />
-                  );
-                })}
-              </div>
+      {cartOverlay ? (
+        <div className="addressForm">
+          <input
+            type="county"
+            class="Cart-form-control"
+            id="cartInputCounty"
+            placeholder="Nome"
+          />
 
-              <div class="CartLine"></div>
-              <div class="CartTotal">
-                <span class="CartFloatLeft">
-                  <div class="CartThin dense">VAT 19%</div>
-                  <div class="CartThin dense">Delivery</div>
-                  TOTAL
-                </span>
-                <span class="CartFloatRight">
-                  <div class="CartThin dense">$68.75</div>
-                  <div class="CartThin dense">$4.95</div>
-                  $435.55
-                </span>
-              </div>
-            </div>
-          </div>
-          <div class="CartCredit-info">
-            <div class="CartCredit-info-content">
-              <form>
-                {deliveries
-                  .filter((del) => del.id_user === currentUser.id_user)
-                  .map((delivery) => {
+          <input
+            type="country"
+            class="Cart-form-control"
+            id="cartInputCountry"
+            placeholder="Cognome"
+          />
+
+          <input
+            type="street"
+            class="Cart-form-control"
+            id="autocomplete"
+            placeholder="Indirizzo"
+          />
+
+          <input
+            type="city"
+            class="Cart-form-control"
+            id="cartInputCity"
+            placeholder="Città"
+          />
+
+          <input
+            type="state"
+            class="Cart-form-control"
+            id="cartInputState"
+            placeholder="Provincia"
+          />
+
+          <input
+            type="zip"
+            class="Cart-form-control"
+            id="cartInputZip"
+            placeholder="CAP"
+          />
+        </div>
+      ) : (
+        <div class="CartContainer">
+          <div class="CartWindow">
+            <div class="CartOrder-info">
+              <div class="CartOrder-info-content">
+                <h2>Riepilogo Ordine</h2>
+                <div className="CartItemsContainer">
+                  {carts.map((item) => {
                     return (
-                      <CartAdress
-                        nome={delivery.first_name}
-                        cognome={delivery.last_name}
-                        indirizzo={delivery.adress}
-                        città={delivery.city}
-                        cap={delivery.cap}
+                      <CartItem
+                        img={item.image_url}
+                        name={item.name}
+                        size={item.size}
+                        color={item.color}
+                        price={item.price}
                       />
                     );
                   })}
-              </form>
+                </div>
 
-              <button class="CartPay-btn">Checkout</button>
+                <div class="CartLine"></div>
+                <div class="CartTotal">
+                  <span class="CartFloatLeft">
+                    <div class="CartThin dense">VAT 19%</div>
+                    <div class="CartThin dense">Delivery</div>
+                    TOTAL
+                  </span>
+                  <span class="CartFloatRight">
+                    <div class="CartThin dense">$68.75</div>
+                    <div class="CartThin dense">$4.95</div>${totalPrice}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="CartCredit-info">
+              <div class="CartCredit-info-content">
+                <form>
+                  {deliveries
+                    .filter((del) => del.id_user === currentUser.id_user)
+                    .map((delivery) => {
+                      return (
+                        <CartAdress
+                          nome={delivery.first_name}
+                          cognome={delivery.last_name}
+                          indirizzo={delivery.adress}
+                          città={delivery.city}
+                          cap={delivery.cap}
+                        />
+                      );
+                    })}
+                </form>
+
+                <button class="CartPay-btn">Checkout</button>
+                <button
+                  class="add-adress CartPay-btn"
+                  onClick={() => setCartOverlay(true)}
+                >
+                  Add Adress
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <Footer />
     </>
   );
